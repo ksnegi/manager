8208ed234eed52c5183fc5e18370da4fa0241fd2
ksnegi <kamal.negi@outlook.com>
2016-11-07 23:28:24 +0530

    Initial commit

---
+++ core/src/main/java/com/negi/manager/dao/cassandra/UserDAOImpl.java
@@ -0,0 +1,34 @@
+package com.negi.manager.dao.cassandra;
+
+import java.util.List;
+
+import com.negi.manager.User;
+import com.negi.manager.dao.UserDAO;
+
+public class UserDAOImpl implements UserDAO {
+
+	@Override
+	public User upsert(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User getById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
